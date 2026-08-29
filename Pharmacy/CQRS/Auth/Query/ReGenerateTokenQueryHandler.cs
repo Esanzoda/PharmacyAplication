fb138cf9d86a380eaf8e92e1fc6fc05025dd2bdc
orash <orashesanov05@gmail.com>
2026-08-28 23:25:38 -0700

    Refactor authentication to use unified User JWT generation

--- Pharmacy/CQRS/Auth/Query/ReGenerateTokenQueryHandler.cs
+++ Pharmacy/CQRS/Auth/Query/ReGenerateTokenQueryHandler.cs
@@ -2,7 +2,6 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Pharmacy.Exception;
 using Pharmacy.Interfaces;
-using Pharmacy.Models.Domain.Enum;
 using Pharmacy.Services.Auth;
 
 namespace Pharmacy.CQRS.Auth.Query;
@@ -43,77 +42,15 @@ public class ReGenerateTokenQueryHandler(
                 "Refresh token expired");
         }
 
-        var newAccessToken = refreshToken.Role switch
-        {
-            Role.Customer => await GenerateCustomerToken(
-                refreshToken.UserId,
-                cancellationToken),
-
-            Role.Employee => await GenerateEmployeeToken(
-                refreshToken.UserId,
-                cancellationToken),
-
-            Role.Deliver or Role.Admin => await GenerateDeliverOrCompanyEmployeeToken(
-                refreshToken.UserId,
-                cancellationToken),
-
-            _ => throw new BusinessException(
-                "Unknown role")
-        };
-        return newAccessToken;
-    }
-
-    private async Task<string> GenerateCustomerToken(
-        long userId,
-        CancellationToken cancellationToken)
-    {
-        var customer = await dbContext.Customers
-            .FirstOrDefaultAsync(
-                x => x.Id == userId,
-                cancellationToken);
-
-        if (customer is null)
-        {
-            throw new ResourceNotFoundException(
-                "Customer not found");
-        }
-
-        return await authService.GenerateTokenForCustomer(customer);
-    }
-
-    private async Task<string> GenerateEmployeeToken(
-        long userId, 
-        CancellationToken cancellationToken)
-    {
-        var employee = await dbContext.Employees
-            .FirstOrDefaultAsync(
-                x => x.Id == userId,
-                cancellationToken);
-
-        if (employee is null)
-        {
-            throw new ResourceNotFoundException(
-                "Employee not found");
-        }
-
-        return await authService.GenerateTokenForEmployee(employee);
-    }
-
-    private async Task<string> GenerateDeliverOrCompanyEmployeeToken(
-        long userId, 
-        CancellationToken cancellationToken)
-    {
         var user = await dbContext.Users
-            .FirstOrDefaultAsync(
-                x => x.Id == userId,
+            .FindAsync([refreshToken.UserId],
                 cancellationToken);
-
         if (user is null)
         {
-            throw new ResourceNotFoundException(
-                "Deliver not found");
+            throw new ResourceNotFoundException("User not found");
         }
 
-        return await authService.GenerateTokenForDeliverOrCompanyEmployee(user);
+        var newAccessToken = await authService.GenerateToken(user);
+        return newAccessToken;
     }
 }
--- Pharmacy/Services/Auth/Auth.cs
+++ Pharmacy/Services/Auth/Auth.cs
@@ -4,8 +4,6 @@ using System.Security.Cryptography;
 using System.Text;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using Pharmacy.CQRS.Customer.Models;
-using Pharmacy.CQRS.Employee.Models;
 using Pharmacy.Infrastructure.Setting;
 using Pharmacy.Models.Domain;
 
@@ -14,9 +12,7 @@ namespace Pharmacy.Services.Auth;
 public interface IAuthService
 {
     Task<string> GenerateRefreshToken();
-    Task<string> GenerateTokenForCustomer(CustomerEntity customer);
-    Task<string> GenerateTokenForEmployee(EmployeeEntity employee);
-    Task<string> GenerateTokenForDeliverOrCompanyEmployee(User deliver);
+    Task<string> GenerateToken(User user);
 }
 
 public class AuthService(IOptionsMonitor<JwtOption> jwt) : IAuthService
@@ -30,67 +26,7 @@ public class AuthService(IOptionsMonitor<JwtOption> jwt) : IAuthService
     }
 
 
-    public Task<string> GenerateTokenForCustomer(CustomerEntity customer)
-    {
-        var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.NameIdentifier, customer.Id.ToString()),
-            new Claim(ClaimTypes.Email, customer.Email),
-            new Claim(ClaimTypes.Role, customer.Role.ToString()),
-            new Claim("Latitude", customer.Latitude.ToString()),
-            new Claim("Longitude", customer.Longitude.ToString()),
-            new Claim(ClaimTypes.StreetAddress, customer.Address)
-        };
-        var key = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(jwt.CurrentValue.SecretKey));
-
-        var credentials =
-            new SigningCredentials(
-                key,
-                SecurityAlgorithms.HmacSha256);
-        var token = new JwtSecurityToken(
-            issuer: jwt.CurrentValue.Issuer,
-            audience: jwt.CurrentValue.Audience,
-            claims: claims,
-            expires:
-            DateTime.UtcNow.AddMinutes(jwt.CurrentValue.AccessTokenExpirationMinutes),
-            signingCredentials:
-            credentials);
-
-        return Task.FromResult(new JwtSecurityTokenHandler()
-            .WriteToken(token));
-    }
-
-    public Task<string> GenerateTokenForEmployee(EmployeeEntity employee)
-    {
-        var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.NameIdentifier, employee.Id.ToString()),
-            new Claim(ClaimTypes.Email, employee.Email),
-            new Claim(ClaimTypes.Role, employee.Position.ToString()),
-            new Claim("PharmacyId", employee.PharmacyId.ToString())
-        };
-        var key = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(jwt.CurrentValue.SecretKey));
-
-        var credentials =
-            new SigningCredentials(
-                key,
-                SecurityAlgorithms.HmacSha256);
-        var token = new JwtSecurityToken(
-            issuer: jwt.CurrentValue.Issuer,
-            audience: jwt.CurrentValue.Audience,
-            claims: claims,
-            expires:
-            DateTime.UtcNow.AddMinutes(jwt.CurrentValue.AccessTokenExpirationMinutes),
-            signingCredentials:
-            credentials);
-
-        return Task.FromResult(new JwtSecurityTokenHandler()
-            .WriteToken(token));
-    }
-
-    public Task<string> GenerateTokenForDeliverOrCompanyEmployee(User user)
+    public Task<string> GenerateToken(User user)
     {
         var claims = new List<Claim>
         {
